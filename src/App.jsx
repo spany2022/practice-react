0c6f8abca1d39f727284e5016b32d9c370894ec5
Sandesh Pany <sandeshpany24@gmail.com>
2024-10-23 08:06:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,3 +17,8 @@ function App() {
 }
 
 export default App;
+
+// File Extensions in React Projects: .jsx is not supported by browsers but works in React projects because of the build process that recognizes JSX code.
+// Using .jsx or .js: Both .jsx and .js can contain JSX code. The choice depends on the project’s build setup. Some projects require .jsx, others use .js with JSX.
+// File Imports: Some projects require the extension in imports (e.g., import App from './App.jsx'), while others do not (import App from './App').
+// No Browser Impact: The file extension doesn't matter to the browser; it's a build process concern.
